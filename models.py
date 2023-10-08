ba3189aa0f1edc72e1a7588a77d53393a86a37c9
PapyNick <panchenko.nikolay.10@gmail>
2023-10-08 10:08:46 +0300

    HW Module 4, Занятие 4, Задание 4

---
+++ models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name="Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price=models.DecimalField("Цена", max_digits=10, decimal_places=2) # ����. 99_999_999.99
+    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Advertisement: id={self.id}, title={self.title}, price={self.price}'
+    class Meta:
+        db_table = 'My_name'
+
