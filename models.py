34deae082ca4003a61d21cc2b80703fef27a98c9
PapyNick <panchenko.nikolay.10@gmail>
2023-10-08 10:11:22 +0300

    HW Module 4, Занятие 4, Задание 4

--- models.py
+++ models.py
@@ -12,5 +12,5 @@ class Advertisement(models.Model):
     def __str__(self):
         return f'Advertisement: id={self.id}, title={self.title}, price={self.price}'
     class Meta:
-        db_table = 'My_name'
+        db_table = 'advertisements'
 
