2edf479e8396e0bdcb35bace5fe3afa3cf21b531
PapyNick <panchenko.nikolay.10@gmail>
2023-10-10 15:24:59 +0300

    Django Project

--- Qq Django/advertisements/app_adverisements/apps.py
+++ Qq Django/advertisements/app_adverisements/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppAdverisementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_adverisements'
+    verbose_name = 'Объявления'
